342418826b846bb1841ebe7813ebc9cfdba51d11
melania27dev <balaceanumelania@yahoo.com>
2025-03-29 16:40:20 +0200

    modified tf-aws.yml and outputs.tf for public ips

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -17,3 +17,8 @@ output "security_group_id" {
   description = "The ID of the security group"
   value       = aws_security_group.sg.id
 }
+
+output "k3s_vm_public_ip" {
+  description = "EC2 K3s IP"
+  value = aws_instance.k3s.public_ip
+}

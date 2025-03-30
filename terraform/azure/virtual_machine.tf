6a09f794802786e7b77d513f0f939629f9c6e003
melania27dev <balaceanumelania@yahoo.com>
2025-03-30 19:36:19 +0300

    changed name to key

--- terraform/azure/virtual_machine.tf
+++ terraform/azure/virtual_machine.tf
@@ -42,8 +42,8 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   provisioner "local-exec" {
     command = <<EOT
     sleep 90
-    chmod 400 ./scripts/devazure.pem
-    ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i ${azurerm_public_ip.my_terraform_public_ip.ip_address}, -u devadmin --private-key=./scripts/devazure.pem ./scripts/install_k3s.yml -vv
+    chmod 400 ./scripts/devazure.pem.pub
+    ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i ${azurerm_public_ip.my_terraform_public_ip.ip_address}, -u devadmin --private-key=./scripts/devazure.pem.pub ./scripts/install_k3s.yml -vv
     EOT
   }
 }

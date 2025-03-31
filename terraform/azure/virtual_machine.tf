7d4b063401148d32a87ef68d3d2975ba803270a3
melania27dev <balaceanumelania@yahoo.com>
2025-03-31 05:14:04 +0300

    automating jenkins to deploy app

--- terraform/azure/virtual_machine.tf
+++ terraform/azure/virtual_machine.tf
@@ -42,6 +42,12 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   }
 }
 
+resource "null_resource" "save_ip" {
+  provisioner "local-exec" {
+    command = "echo ${azurerm_public_ip.my_terraform_public_ip.ip_address} > ip_k3s.txt"
+  }
+}
+
 
 #   provisioner "local-exec" {
 #     command = <<EOT
